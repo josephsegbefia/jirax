769dabcdd127c7a7a54ef2e7cc1f37b5e9f0404b
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-10-13 01:05:29 +0000

    Finish signin form

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,10 @@
+"use client";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export const SignInCard = () => {
   return (
@@ -10,7 +15,55 @@ export const SignInCard = () => {
       <div className="px-7">
         <DottedSeparator />
       </div>
-      <CardContent></CardContent>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            type="email"
+            required
+            value=""
+            onChange={() => {}}
+            placeholder="Enter email address"
+            disabled={false}
+          />
+          <Input
+            type="password"
+            required
+            value=""
+            onChange={() => {}}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={256}
+          />
+          <Button disabled={false} size="lg" className="w-full">
+            Sign In
+          </Button>
+        </form>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          size="lg"
+          className="w-full"
+          disabled={false}
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Log in with Google
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          className="w-full"
+          disabled={false}
+        >
+          <FaGithub className="mr-2 size-5" />
+          Log in with GitHub
+        </Button>
+      </CardContent>
     </Card>
   );
 };
