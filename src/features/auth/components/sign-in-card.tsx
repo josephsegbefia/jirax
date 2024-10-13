b36309174b7c6fe82d7486c69058b46e82168890
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-10-13 00:21:47 +0000

    Do dotted separator

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,11 +1,16 @@
+import { DottedSeparator } from "@/components/dotted-separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const SignInCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader>
-        <CardTitle>Welcome back!</CardTitle>
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
       </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent></CardContent>
     </Card>
   );
 };
