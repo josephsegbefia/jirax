fd93a981a7752713398ace798346fa8c9c12ce6d
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-10-14 12:37:33 +0000

    Do logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-// import Image from "next/image";
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex gap-4">
-      <Button>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritrary">Teritrary</Button>
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
 
-      <Input />
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+  return (
+    <div>
+      Page is only visible to authorized users{" "}
+      <Button onClick={() => mutate()}>Logout </Button>
     </div>
   );
 }
